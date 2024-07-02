9f53c6dda96bb0f182672a39b29b431df475c00c
Wilyanse <willjose51@gmail.com>
2024-07-03 04:16:20 +0800

    Import images to svelte file

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Wrapper from "./Wrapper.svelte";
-
+    import outlineImg from '$lib/images/bigLogo.png';
 </script>
 <Wrapper id="Landing Page">
-    Landing Page
+    <div class="flex flex-col">
+        <img src={outlineImg} alt="Outline logo" />
+    </div>
 </Wrapper>
