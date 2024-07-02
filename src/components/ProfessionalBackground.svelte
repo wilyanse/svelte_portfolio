33ce1871a317c4b09b5b8200932c7dd7b52661d7
Wilyanse <willjose51@gmail.com>
2024-07-02 19:49:48 +0800

    Add placeholder sections

---
+++ src/components/ProfessionalBackground.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Wrapper from "./Wrapper.svelte";
+
+</script>
+<Wrapper id="Professional Background">
+    Professional Background
+</Wrapper>
---
+++ src/components/SkillsInterests.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Wrapper from "./Wrapper.svelte";
+
+</script>
+<Wrapper id="SkillsInterests">
+    Skills & Interests
+</Wrapper>
