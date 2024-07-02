85714cb7a5743b6cf4129b01c99ee42f91dc461f
Wilyanse <willjose51@gmail.com>
2024-07-03 04:25:00 +0800

    Add logo

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -3,7 +3,14 @@
     import outlineImg from '$lib/images/bigLogo.png';
 </script>
 <Wrapper id="Landing Page">
-    <div class="flex flex-col">
+    <div class="flex flex-row m-10">
         <img src={outlineImg} alt="Outline logo" />
+        <h1>Hi! I see you've made it to my portfolio</h1>
     </div>
 </Wrapper>
+
+<style lang="postcss">
+    img {
+        @apply aspect-square size-1/2;
+    }
+</style>
