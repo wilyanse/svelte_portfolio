0c47e8378c296be63d408a3137a8416df9381d7f
Wilyanse <willjose51@gmail.com>
2024-07-03 18:53:25 +0800

    Finish left side of landing page

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -4,11 +4,11 @@
 </script>
 <Wrapper id="Landing Page">
     <div class="grid grid-cols-3 min-h-full m-10 space-x-5 place-items-center p-10 min-w-full">
-        <div>
-            <h1>Hi!</h1>
-            <hr class="!border-t-8 !border-double" />
-            <h2>I see you've made it to my portfolio.</h2>
-            <button>Contact Me</button>
+        <div class="min-h-full space-y-5 grid grid-rows-4">
+            <h1>Hi.</h1>
+            <hr class="!border-t-8 !border-double bg-white" />
+            <h2 class="row-span-2">You've made it to my portfolio.</h2>
+            <a href="/">Contact Me</a>
         </div>
 
         <img src={outlineImg} alt="Outline logo" />
@@ -19,5 +19,17 @@
 <style lang="postcss">
     img {
         @apply aspect-square size-1/2 min-w-full h-auto;
+    }
+
+    h1 {
+        @apply h1 font-bold text-9xl;
+    }
+    
+    h2 {
+        @apply h2 font-semibold;
+    }
+
+    a {
+        @apply btn variant-ghost-tertiary max-w-xs text-xl font-bold self-center mx-auto;
     }
 </style>
