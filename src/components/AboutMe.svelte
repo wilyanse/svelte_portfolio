009ce727e85ea719419f1721e5b8816a8a5130fb
Wilyanse <willjose51@gmail.com>
2024-07-04 04:09:55 +0800

    About Me: initial design

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -15,8 +15,8 @@
                 I've worked in several fields of computer science, which you will find out about the more that you scroll through this portfolio website.
             </h3>
         </div>
-        <div class="row-span-3 row-start-3 grid grid-cols-2">
-            <div>
+        <div class="row-span-3 row-start-3 grid grid-cols-2 space-x-10">
+            <div class="variant-ghost-primary w-full">
                 <h2 class="font-semibold p-5">What have I <strong>done?</strong></h2>
                 <div class="p-5 space-y-5">
                     <div class="flex flex-row space-x-5">
@@ -33,19 +33,19 @@
                     </div>
                 </div>
             </div>
-            <div>
+            <div class="variant-ghost-primary w-full">
                 <h2 class="font-semibold p-5">What do I <strong>aim to do?</strong></h2>
                 <div class="p-5 space-y-5">
                     <div class="flex flex-row space-x-5">
-                        
+                        <svg class="h-8 w-8 text-tertiary-300"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <ellipse cx="12" cy="6" rx="8" ry="3"></ellipse>  <path d="M4 6v6a8 3 0 0 0 16 0v-6" />  <path d="M4 12v6a8 3 0 0 0 16 0v-6" /></svg>
                         <h3>Work with data <strong>professionally</strong></h3>
                     </div>
                     <div class="flex flex-row space-x-5">
-
-                        <h3><strong>Develop my skills</strong> in other fields of Computer Science as a hobby</h3>
+                        <svg class="h-8 w-8 text-tertiary-300"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />  <polyline points="17 6 23 6 23 12" /></svg>
+                        <h3><strong>Develop my skills</strong> as a hobby</h3>
                     </div>
                     <div class="flex flex-row space-x-5">
-
+                        <svg class="h-8 w-8 text-tertiary-300"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="9" cy="7" r="4" />  <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />  <line x1="19" y1="7" x2="19" y2="10" />  <line x1="19" y1="14" x2="19" y2="14.01" /></svg>
                         <h3><strong>Leave my mark on the world!</strong></h3>
                     </div>
                 </div>
