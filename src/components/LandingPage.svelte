9aef7263693f850796f7de2e81b956af6542e060
Wilyanse <willjose51@gmail.com>
2024-07-03 18:24:33 +0800

    Add content to left side of landing page

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -3,10 +3,16 @@
     import outlineImg from '$lib/images/bigLogo.png';
 </script>
 <Wrapper id="Landing Page">
-    <div class="grid grid-cols-3 min-h-full m-10 space-x-5 place-items-center">
-        <h1>Will Jose</h1>
+    <div class="grid grid-cols-3 min-h-full m-10 space-x-5 place-items-center p-10 min-w-full">
+        <div>
+            <h1>Hi!</h1>
+            <hr class="!border-t-8 !border-double" />
+            <h2>I see you've made it to my portfolio.</h2>
+            <button>Contact Me</button>
+        </div>
+
         <img src={outlineImg} alt="Outline logo" />
-        <h1>Hi! I see you've made it to my portfolio</h1>
+        <h1>Will Jose. At your service.</h1>
     </div>
 </Wrapper>
 
