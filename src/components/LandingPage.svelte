0489df64427eb991f52e59e83beef7cee4289f73
Wilyanse <willjose51@gmail.com>
2024-07-03 18:13:56 +0800

    Center items

--- src/components/LandingPage.svelte
+++ src/components/LandingPage.svelte
@@ -3,7 +3,8 @@
     import outlineImg from '$lib/images/bigLogo.png';
 </script>
 <Wrapper id="Landing Page">
-    <div class="flex flex-row m-10">
+    <div class="grid grid-cols-3 min-h-full m-10 space-x-5 place-items-center">
+        <h1>Will Jose</h1>
         <img src={outlineImg} alt="Outline logo" />
         <h1>Hi! I see you've made it to my portfolio</h1>
     </div>
@@ -11,6 +12,6 @@
 
 <style lang="postcss">
     img {
-        @apply aspect-square size-1/2;
+        @apply aspect-square size-1/2 min-w-full h-auto;
     }
 </style>
