5135db0788e2ae03201de3ede0a863dd3d12f1b2
Wilyanse <willjose51@gmail.com>
2024-07-04 16:43:04 +0800

    ProfessionalBG: add reading data from JSON file

--- src/components/ProfessionalBackground.svelte
+++ src/components/ProfessionalBackground.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Wrapper from "./Wrapper.svelte";
+    import data from "$lib/data/work_exp.json";
 
+    console.log(data);
 </script>
 <Wrapper id="Professional Background">
     Professional Background
