fe78d87ed357c45e02827387c34745e017048aa9
Wilyanse <willjose51@gmail.com>
2024-07-05 23:58:38 +0800

    General: changed metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,10 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <!-- App Shell -->
