9283361614b5f6221b19b3c821d3e9ea47affa41
Wilyanse <willjose51@gmail.com>
2024-07-05 21:30:17 +0800

    Projects: finished initial layout

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,7 +1,107 @@
-<script>
+<script lang="ts">
 	import Wrapper from "./Wrapper.svelte";
 
+    import { Autocomplete } from '@skeletonlabs/skeleton';
+    import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+
+    import { popup, type PopupSettings } from "@skeletonlabs/skeleton";
+
+    import projectsData from "$lib/data/projects.json";
+
+    import { InputChip } from '@skeletonlabs/skeleton';
+
+    type FlavorOption = AutocompleteOption<string, { healthy: boolean }>;
+    const flavorOptions: AutocompleteOption<string>[] = [
+        { label: 'keyword_0_0', value: 'keyword_0_0', keywords: 'plain, basic' },
+    ];
+
+	function onInputChipSelect(event: CustomEvent<FlavorOption>): void {
+		console.log('onInputChipSelect', event.detail);
+		if (inputChipList.includes(event.detail.value) === false) {
+			inputChipList = [...inputChipList, event.detail.value];
+			inputChip = '';
+		}
+	}
+
+    let inputChip = '';
+
+    let inputChipList: string[] = [];
+
+    function hasCommonElement(arr1: string[], arr2: string[]): boolean {
+        // Convert the first array to a Set for O(1) lookup time
+        const set1 = new Set(arr1);
+
+        // Iterate through the second array and check for any common element
+        for (const element of arr2) {
+            if (set1.has(element)) {
+                return true; // Return true as soon as a common element is found
+            }
+        }
+
+        return false; // Return false if no common elements are found
+    }
 </script>
 <Wrapper id="Projects">
-    Projects
+    <div class="max-h-screen max-w-screen-lg p-2.5 flex flex-col gap-4 place-items-center space-y-3">
+        <div class="variant-ghost-secondary p-10 self-center mx-auto">
+            <h1>PROJECTS</h1>
+        </div>
+        <div class="w-full">
+            <InputChip bind:input={inputChip} bind:value={inputChipList} name="chips" />
+            <div class="card w-full h-32 p-4 overflow-y-auto" tabindex="-1">
+                <Autocomplete
+                    bind:input={inputChip}
+                    options={flavorOptions}
+                    denylist={inputChipList}
+                    on:selection={onInputChipSelect}
+                />
+            </div>
+        </div>
+        <div class="max-w-screen-lg w-screen">
+            {#if inputChipList.length > 0}
+            <div class="flex snap-x scroll-px-4 snap-mandatory scroll-smooth gap-4 overflow-x-auto px-4 py-2 place-content-center">
+                {#each Object.entries(projectsData) as [id, fields]}
+                    {#if hasCommonElement(inputChipList, fields.keywords)}
+                        <div class="snap-start shrink-0 card py-5 w-80 h-80 text-center variant variant-ghost-primary grid grid-rows-5">
+                            <header class="card-header m-auto">
+                                <h3>
+                                    {fields.name}
+                                </h3>
+                                
+                            </header>
+                            <section class="p-4 space-y-1.5 mx-auto">
+                                <a href={fields.link}>Github Link</a>
+                            </section>
+                            <footer class="card-footer mx-auto row-span-3">
+                                <h5>{fields.desc}</h5>
+                            </footer>
+                        </div>
+                    {/if}
+                {/each}
+            </div>
+            {:else}
+                <div class="w-full h-80 place-content-center">
+                    <h1>Try clicking on the keywords to get a list of projects</h1>
+                </div>
+            {/if}           
+        </div>
+        <div>
+            <a href="/" class="btn variant-ghost-secondary text-xl font-bold self-center mx-auto">What about my personal projects?</a>
+        </div>
+    </div>
 </Wrapper>
+
+<style lang="postcss">
+    h1 {
+        @apply font-bold text-5xl h1 text-center;
+    }
+
+    h2 {
+        @apply h2 text-xl font-semibold;
+    }
+
+    h3 {
+        @apply h3 text-lg font-semibold;
+    }
+
+</style>
