80c53c1288e7e89723218ff8469a6b7fc44cb8fd
Wilyanse <willjose51@gmail.com>
2024-07-05 23:17:11 +0800

    Contact: add contact details

--- src/components/SkillsInterests.svelte
+++ src/components/SkillsInterests.svelte
@@ -3,5 +3,17 @@
 
 </script>
 <Wrapper id="SkillsInterests">
-    Skills & Interests
+    <div class="flex flex-col">
+        <div class="variant-ghost-secondary p-10 self-center mx-auto">
+            <h1>Skills & Interests</h1>
+        </div>
+        
+    </div>
+
 </Wrapper>
+
+<style lang="postcss">
+    h1 {
+        @apply font-bold text-5xl h1 uppercase;
+    }
+</style>
